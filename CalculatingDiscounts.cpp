4bf80035cc37ce7c31ec3cf31d4427f8640b62d8
ElliotBruhl <166664797+ElliotBruhl@users.noreply.github.com>
2024-08-23 13:10:16 -0400

    Add files via upload

---
+++ CalculatingDiscounts.cpp
@@ -0,0 +1,16 @@
+//This program calculates and displays the discounted value of an item
+#include <iostream>
+using namespace std;
+
+int main() {
+    double regularPrice, percentDiscount, discountAmount, reducedPrice;
+    regularPrice = 59.95;
+    percentDiscount = 0.2;
+    discountAmount = regularPrice * percentDiscount;
+    reducedPrice = regularPrice - discountAmount;
+
+    cout << "The discount amount is $" << discountAmount << endl;
+    cout << "The discounted price is $" << reducedPrice;
+
+    return 0;
+}
